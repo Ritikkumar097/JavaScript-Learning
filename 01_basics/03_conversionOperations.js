f21bfdf63ac4eeb900c08f64309d46101b193b6f
Ritikkumar097 <ritik584656@gmail.com>
2026-02-13 22:21:14 +0530

    conversionOperation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,70 @@
+// let score = "36"
+
+// console.log(typeof score);
+// console.log(typeof (score);)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// now convert 33abc to number
+
+// let string1 = "33abc"
+// console.log("dataType of string1: ",typeof string1);
+
+// let convertToNumber = Number(string1)
+// console.log("dataType of converttonumber: ",typeof convertToNumber);
+// console.log(convertToNumber);
+
+// now convert 1 into boolean
+
+// let isLoggedIn = 1
+// console.log("dataType of isLoggedIn: ",typeof isLoggedIn);
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log("dataType of booleanIsLoggedIn: ",typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// now convert 33 into string
+
+// let someNumber = 33
+
+// let stringSomeNumber = String(someNumber)
+// console.log("dataType of stringSomeNumber: ",typeof stringSomeNumber);
+// console.log("stringSomeNumber: ",stringSomeNumber);
+
+
+// ************************** Operations **********************************
+
+// let value = 3
+// let negValue = -value    // makeing negative value
+
+// console.log(3+8);
+// console.log(3-8);
+// console.log(3*8);
+// console.log(3**8);   // this means 3 to the power 8
+// console.log(3/8);
+// console.log(3%8);   // this is modulo operator which give remainder
+
+// let str1 = "Hello"
+// let str2 = " World"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(2 + "3");
+// console.log("2" + 3);
+// console.log("2" + "3")
+// console.log("1" + 2 + 3);
+// console.log("1" + (2 + 3));
+// console.log(1 + 2 + "3");
+
+
+
+console.log(true);
+console.log(+true);   // gives output: 1 because + convert operand(true) into number and Since true menas 1, it prints 1
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter)
+
